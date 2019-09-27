8067e184db4ecc121d1fbe0c7e1542bce07e996b
udon <skynetx002@gmail.com>
2019-09-27 14:36:53 +0700

    First Commitgit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: My First Plugin Demo // Tên của plugin
+ * Plugin URI: http://hocwp.net // Địa chỉ trang chủ của plugin
+ * Description: Đây là plugin đầu tiên mà tôi viết dành riêng cho WordPress, chỉ để học tập mà thôi. // Phần mô tả cho plugin
+ * Version: 1.0 // Đây là phiên bản đầu tiên của plugin
+ * Author: Sau Hi // Tên tác giả, người thực hiện plugin này
+ * Author URI: http://sauhi.com // Địa chỉ trang chủ của tác giả
+ * License: GPLv2 or later // Thông tin license của plugin, nếu không quan tâm thì bạn cứ để GPLv2 vào đây
+ */
+?>
